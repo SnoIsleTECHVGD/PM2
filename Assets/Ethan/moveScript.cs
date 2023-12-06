925c4712b7656fc3399e5a3ba605c61573a6a974
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2023-12-06 13:24:50 -0800

    Added a WIP Pause menu

--- Assets/Ethan/moveScript.cs
+++ Assets/Ethan/moveScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class moveScript : MonoBehaviour
 {
+    public KeyCode MoveUp;
+    public KeyCode MoveDown;
+    public KeyCode MoveLeft;
+    public KeyCode MoveRight;
+    public KeyCode Dash;
+    public float speed;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+        //Movement script
+        if (Input.GetKey(MoveUp))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        }
+        if (Input.GetKey(MoveDown))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+        }
+        if (Input.GetKey(MoveLeft))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+        }
+        if (Input.GetKey(MoveRight))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        }
+        //Dash script
+        //if (Input.GetKeyDown(Dash))
+      
+
     }
 }
