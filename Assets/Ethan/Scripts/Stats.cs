6d0fbb9c02982cbf5bd850a42cfb68a37ac70a5f
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2024-01-04 13:08:56 -0800

    Made prefabs for Player & Camera

---
+++ Assets/Ethan/Scripts/Stats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+
+    public float PlayerHP;
+    
+    // Start is called before the first frame update
+    private void Update()
+    {
+
+    }
+
+
+}
---
+++ Assets/Ethan/Scripts/WeaponParent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParent : MonoBehaviour
+{/*
+    public Vector2 PointerPosition { get; set; }
+
+    private Vector2 pointerInput;
+
+    private void Update()
+    {
+        pointerInput = GetPointerInput();
+        transform.right = (PointerPosition - (Vector2)transform.position).normalized;
+    }
+    */
+}
