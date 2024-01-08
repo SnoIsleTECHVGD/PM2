4143aa0a1f855587eaef0fa8c50b7d4a2f751908
MUKILTEO\1717794 <1717794@mukilteo.wednet.edu>
2024-01-08 12:18:54 -0800

    Added stuff idk

--- Assets/Ethan/Scripts/Stats.cs
+++ Assets/Ethan/Scripts/Stats.cs
@@ -6,5 +6,14 @@ public class Stats : MonoBehaviour
 {
     public int health;
     public int attack;
-    
+
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);  
+
+        }
+    }
 }
