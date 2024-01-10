5864f7729f0e9d95b555d8296550cd20da0c17b6
MUKILTEO\1717222 <1717222@mukilteo.wednet.edu>
2024-01-10 13:27:21 -0800

    Working on dialogue

--- Assets/Toby/Toby/Scripts/DialogueManager.cs
+++ Assets/Toby/Toby/Scripts/DialogueManager.cs
@@ -1,14 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
-    private Queue<string> sentences; 
+    public Text dialogueText;
+
+    private Queue<string> sentences;
     void Start()
     {
         sentences = new Queue<string>();
     }
 
-  
+    public void StartDialogue(ObjectDialogue dialogue)
+    {
+        
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence () 
+    { 
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+    }
+
+    
+    
 }
+
+
+  
+
--- Assets/Toby/Toby/Scripts/ObjectDialogue.cs
+++ Assets/Toby/Toby/Scripts/ObjectDialogue.cs
@@ -6,5 +6,7 @@ using UnityEngine;
 public class ObjectDialogue
 {
     public string name;
+
+    [TextArea(3,10)]
     public string[] sentences; 
 }
