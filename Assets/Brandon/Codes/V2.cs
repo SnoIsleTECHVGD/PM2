a38e21ed317b598bc277f9e1734d007deaeb7934
MUKILTEO\1600555 <1600555@mukilteo.wednet.edu>
2024-01-11 11:38:03 -0800

    Fixed enemy animations, potential problems with player animations

--- Assets/Brandon/Codes/V2.cs
+++ Assets/Brandon/Codes/V2.cs
@@ -20,12 +20,9 @@ public class V2 : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-       
         {
             GetComponent <Rigidbody2D> ().velocity = (this.transform.position - player.transform.position) * speed;
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
         }
     }
 }
