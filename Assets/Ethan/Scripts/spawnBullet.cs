0b111ba8555cd432fe305e1b1aa25eedc3d8d455
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2024-09-17 11:19:13 -0700

    idk sumthin to do with guns shooting bullets

--- Assets/Ethan/Scripts/spawnBullet.cs
+++ Assets/Ethan/Scripts/spawnBullet.cs
@@ -9,15 +9,16 @@ public class spawnBullet : MonoBehaviour
     public GameObject theCamera;
     public Vector3 HELPPP;
     public int bulletSpeed = 5;
-    
+    public int bulletDelayTime;
     // Update is called once per frame
     void Update()
     {
         HELPPP = Input.mousePosition;
         mousePos = theCamera.transform.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
+
             Vector2 target = theCamera.transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
             Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
             Vector2 direction = target - myPos;
@@ -26,9 +27,14 @@ public class spawnBullet : MonoBehaviour
             GameObject projectile = Instantiate(bullet, transform.position, rotation);
             projectile.SetActive(true);
             projectile.GetComponent<Rigidbody2D>().velocity = direction * 5 * bulletSpeed;
-            
+            StartCoroutine(bulletDelay());
         }
-        
+    }
+    IEnumerator bulletDelay()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(bulletDelayTime);
+        print(Time.time);
     }
    
 }
