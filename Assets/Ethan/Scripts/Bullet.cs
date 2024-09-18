54c0bd817f2a0024902d3366954cb0409774c21c
MUKILTEO\1717794 <hypercontrlrtwitch@gmail.com>
2024-09-18 13:25:52 -0700

    did some bullet delay schtuff

--- Assets/Ethan/Scripts/Bullet.cs
+++ Assets/Ethan/Scripts/Bullet.cs
@@ -8,16 +8,17 @@ public class Bullet : MonoBehaviour
     public cameraShake cameraShake;
     public float speed = 20f;
     public Rigidbody2D rb;
+    
     private void Start()
     {
         rb.velocity = transform.right * speed;
        
     }
-    private void OnCollisionEnter2D(Collider2D hitInfo)
+    private void OnCollisionEnter2D(Collision2D hitInfo)
     {
-        Debug.Log("Please work");
-        Debug.Log(hitInfo.name);
+        
         Destroy(gameObject);
         StartCoroutine(Camera.main.GetComponent<cameraShake>().Shake(.15f, .4f));
     }
+    
 }
--- Assets/Ethan/Scripts/gun.cs
+++ Assets/Ethan/Scripts/gun.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class gun : MonoBehaviour
 {
+    public float bulletDelayTime;
     public Transform firepoint;
     public GameObject bulletPrefab;
     // Update is called once per frame
     void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
+    {//if get button AND other variable called like "has fired" or smth
+        if(Input.GetButton("Fire1"))
         {
             Shoot();
+            StartCoroutine(bulletDelay());
+            
         }
     }
     void Shoot()
     {
         //shooting logic
         Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+    }
+    IEnumerator bulletDelay()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(bulletDelayTime);
+        print(Time.time);
     }
 }
